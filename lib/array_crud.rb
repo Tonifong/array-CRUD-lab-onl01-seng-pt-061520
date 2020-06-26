12e046836b20d68f13cc8048d83531ae497d1aff
Antonio O Fong <omarquinzan@yahoo.com>
2020-06-26 21:45:07 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,12 +1,16 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["San", "Daniel", "Sadan", "Jimmy" ]
 end
 
+require 'pry'
 def add_element_to_end_of_array(array, element)
+  array=["wow", "I", "am", "really", "learning"]
+  element= "arrays!"
+  binding.pry
   
 end
 
@@ -15,6 +19,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
+  
   
 end
 
